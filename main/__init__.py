490595c366a96ea2369ccbf631ddb9ce974a53d5
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-04-06 20:18:42 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,1 +1,25 @@
+""© MrMKN.git"""
+
+from pyrogram import Client
+from main.config import Config
+
+
+class Bot(Client): 
+    def __init__(self):
+        super().__init__(
+            name="multi-accepter",
+            session_string=Config.BOT_SESSION,          
+            workers=100,
+        )
+     
+    async def start(self):
+        await super().start()                      
+        me = await self.get_me()                     
+        print("bot started......")
+        
+    async def stop(self, *args):       
+        await super().stop()
+        print("Bot stopped. Bye.")
+
+
 
