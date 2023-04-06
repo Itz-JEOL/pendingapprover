f6dfee2283d28e20f2dbbe9a40fd31247a12783d
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-04-06 20:21:26 +0530

    Update config.py

--- main/config.py
+++ main/config.py
@@ -1,1 +1,17 @@
+import os
 
+class Config:
+    API_ID = int(14417186)
+    API_HASH = "21731d919d79f78de24bdf1f6ccd6921"
+    BOT_TOKEN = "5989405319:AAEqwAGfViL0aNNykXvX0D-bPRnxQc968zQ"
+    BOT_SESSION = ""
+
+    DB_URL = ""
+    DB_NAME = ""
+
+    LOG_CHANNEL = int()
+    ADMINS = []
+   
+
+class Txt(object):
+    ":+-"
