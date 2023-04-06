963b87bb66fc3ac87348f6a6c246fc45ccfb5b28
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-04-06 20:10:41 +0530

    Update accept.py

--- main/accept.py
+++ main/accept.py
@@ -1,4 +1,81 @@
+"""© @MrMKN.git"""
+
+# pyro module 
 from pyrogram import Client, filters
+from pyrogram.enums import ParseMode, ChatType, ChatMemberStatus
+from pyrogram.errors import FloodWait
+
+from config import Config, Txt
+from database import db
+
+import asyncio
+
+@Client.on_message(filters.command("run") & ( filters.channel | filters.group ))
+async def _accept(bot, msg):
+    chat_id = msg.chat.id
+    chat_type = msg.chat.type
+    chat_title = msg.chat.title
+    
+    if chat_type in [ChatType.GROUP, ChatType.SUPERGROUP]:
+        if msg.from_user:
+            user = msg.from_user.id 
+            get_user = await client.get_chat_member(
+                chat_id=chat_id,
+                user_id=user
+            )
+            if get_user.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:                
+                _ex = await msg.reply(
+                    text=Txt.NON_ADMIN
+                )
+                await asyncio.sleep(3)
+                await _ex.delete(True)
+                return
+
+    _userSession = await db.get_client(chat_id)
+
+    if _userSession is None:
+        empty_string = await msg.reply(
+            text=Txt.NO_SESSION,
+            parse_mode=ParseMode.DEFAULT,
+            disable_web_page_preview=True
+        )                
+        await asyncio.sleep(3)
+        await empty_string.delete(True)
+        return
+
+    try: 
+        UserBot = Client(
+            name=":memory:",
+            session_string=_userSession,
+            in_memory=True
+        )
+    except Exception as e:
+        Config.LOGGER.error(e)
+        await msg.reply_text(e)
+        return
+
+    try:
+       while True: # create loop is better techniq 🙃
+           try:
+               await UserBot.approve_all_chat_join_requests(chat_id)         
+           except FloodWait as t:
+               asyncio.sleep(t.value)
+               await UserBot.approve_all_chat_join_requests(chat_id) 
+           except Exception as e:
+               Config.LOGGER.error(e)
+    except FloodWait as s:
+        asyncio.sleep(s.value)
+        while True:
+           try:
+               await UserBot.approve_all_chat_join_requests(chat_id)         
+           except FloodWait as t:
+               asyncio.sleep(t.value)
+               await UserBot.approve_all_chat_join_requests(chat_id) 
+           except Exception as e:
+               Config.LOGGER.error(e)
+    else:
+        await bot.send_message(chat_id, Txt.FINISHED.format(chat_title))
+    
 
 
 
@@ -6,8 +83,13 @@ from pyrogram import Client, filters
 
 
 
-async def accept(bot, msg):
-    user_bot = await db.get_client(user_id)
-    if user_bot is None:
-        return await m.reply(text="Sorry Dude You Don't Have UserBot place add user session")                
 
+
+
+
+
+
+
+
+
+                         
