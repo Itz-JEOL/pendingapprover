7362e449369ab16acf46bfd88d516c584af6b76e
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-04-06 18:01:31 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,29 +1,58 @@
-from pymongo import MongoClient
+import motor.motor_asyncio
 
-client = MongoClient("mongodb+srv://MONGOO:MONGOO@cluster0.0a5cpqh.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = "mongodb+srv://MONGOO:MONGOO@cluster0.0a5cpqh.mongodb.net/?retryWrites=true&w=majority"        
+DB_NAME = "Itz-Jeol"
 
-users = client['main']['bots']
+class Database:
+    
+    def __init__(self):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(DB_URL)
+        self.db = self._client[DB_NAME]
+        self.col = self.db.users
+
+    def new_user(self, id):
+        return dict(
+            _id=id,
+            id=id,            
+            session=None,
+            chat=None,          
+        )
+
+    async def add_user(self, id):
+        user = self.new_user(id)
+        await self.col.insert_one(user)
+
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return True if user else False
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+  
+    async def get_all_users(self):
+        all_users = self.col.find({})
+        return all_users
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def set_client(self, id, session):
+        await self.col.update_one({'id': int(id)}, {'$set': {'session': session}})
+
+    async def get_client(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('session', None)
+
+    async def set_chat(self, id, chat):
+        await self.col.update_one({'id': int(id)}, {'$set': {'chat': chat}})
+
+    async def get_chat(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('chat', None)
 
 
-def already_db(user_id):
-        user = users.find_one({"session_string" : str(user_id)})
-        if not user:
-            return False
-        return True
 
-def add_user(user_id):
-    in_db = already_db(user_id)
-    if in_db:
-        return
-    return users.insert_one({"session_string": str(user_id)}) 
+db = Database()
 
-def remove_user(user_id):
-    in_db = already_db(user_id)
-    if not in_db:
-        return 
-    return users.delete_one({"session_string": str(user_id)})
 
-def all_users():
-    user = users.find({})
-    usrs = len(list(user))
-    return usrs
