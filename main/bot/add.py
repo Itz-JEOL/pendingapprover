789faf75160f7983b2dcc42b4e868d6b9d6a93b2
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-04-06 22:09:00 +0530

    Update add.py

--- main/bot/add.py
+++ main/bot/add.py
@@ -1,16 +1,20 @@
 """© @MrMKN.git"""
 
-from pyrogram import filters, types
+# part of pyro
+from pyrogram import filters
+from pyrogram.errors import PeerIdInvalid
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import *
+
 
 from database import db
 
 
 
-@Bot.on_message(filters.command("add") & filters.private)
+@Bot.on_message(filters.command("add_chat") & filters.private)
 async def _add_user(bot: Bot, msg: types.Message):
   
-    user_id = msg.from_user.id
-    
+    user_id = msg.from_user.id    
     chat = await bot.ask_message(
         chat_id=user_id,
         text="please send your (channel / group) id",       
@@ -23,16 +27,28 @@ async def _add_user(bot: Bot, msg: types.Message):
         return await chat.continue_propagation()
   
     try:
-        get_chat = await bot.get_chat(int(chat.text))
-    except:
+        chat_id = int(chat.text)
+        m_st = bot.get_chat_member(chat_id, user_id)
+        if m_st.status == ChatMemberStatus.MEMBER:
+            return await msg.reply_text("**you're not admin in this chat**")
+        b_st = bot.get_chat_member(chat_id, "me")
+        if b_st.status == ChatMemberStatus.ADMINISTRATOR:
+            get_chat = await bot.get_chat(chat_id)
+            added = await db.add_chat(user_id, get_chat.id)
+            if added:
+                await chat.reply_text("✅️ Your Channel Added")
+                return await chat.continue_propagation()
+            else:             
+                await chat.reply("your channel is already added")
+                return await chat.continue_propagation()          
+    except PeerIdInvalid:
         await chat.reply("wrong chat id. Process Cancelled")
         return await chat.continue_propagation()
 
-    if await db.get_chat(int(chat.text)):
-        return await chat.reply("your channel is already added")
+    
+    
+@Client.on_propagation(*args = True)
 
-    await db.add_chat(int(chat.text))
-    await chat.continue_propagation()
 
 
 
