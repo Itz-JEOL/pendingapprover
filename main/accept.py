96327cac9550ab4752a1fb1304ea3872a5eefdef
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-04-06 18:34:09 +0530

    Update accept.py

--- main/accept.py
+++ main/accept.py
@@ -1,1 +1,13 @@
+from pyrogram import Client, filters
+
+
+
+
+
+
+
+async def accept(bot, msg):
+    user_bot = await db.get_client(user_id)
+    if user_bot is None:
+        return await m.reply(text="Sorry Dude You Don't Have UserBot place add user session")                
 
