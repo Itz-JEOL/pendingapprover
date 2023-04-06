72e6ddca8931f05a3970a5f5bacb936c838595e2
Jeol Paul <123471314+Itz-JEOL@users.noreply.github.com>
2023-04-06 12:13:41 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,9 +1,42 @@
 from pyrogram import Client
 from pyrogram.types import *
 from pyrogram import filters
-
+from os import environ
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.errors import FloodWait
 
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client: Client, message: Message):
     await message.reply("Hey! It's Just a Cloner Bot example source Code")
+
+    
+@Client.on_message(filters.command(["run", "approve"], [".", "/"]))                     
+async def approve(client: User, message: Message):
+    Id = message.chat.id
+    await message.delete(True)
+ 
+    try:
+       while True: # create loop is better techniq 🙃
+           try:
+               await client.approve_all_chat_join_requests(Id)         
+           except FloodWait as t:
+               asyncio.sleep(t.value)
+               await client.approve_all_chat_join_requests(Id) 
+           except Exception as e:
+               logging.error(str(e))
+    except FloodWait as s:
+        asyncio.sleep(s.value)
+        while True:
+           try:
+               await client.approve_all_chat_join_requests(Id)         
+           except FloodWait as t:
+               asyncio.sleep(t.value)
+               await client.approve_all_chat_join_requests(Id) 
+           except Exception as e:
+               logging.error(str(e))
+
+    msg = await client.send_message(Id, "**Task Completed** ✓ **Approved Pending All Join Request**")
+    await asyncio.sleep(3)
+    await msg.delete()    
