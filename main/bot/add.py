7a436d71227557352d67320132b3307f209187d2
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-04-08 01:45:34 +0530

    Update add.py

--- main/bot/add.py
+++ main/bot/add.py
@@ -14,7 +14,7 @@ from main import Bot
 async def _add_user(bot: Bot, msg: Message):
   
     user_id = msg.from_user.id
-    already = db.get_chat(user_id) 
+    already = await db.get_chat(user_id) 
     if await already['chat'] is not None:
         return await msg.reply_text("sorry you have already a connection")
 
