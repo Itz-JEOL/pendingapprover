fef30399a89149ddd726e41ea3c183635dc5b38f
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-04-08 01:24:46 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+apt update && apt upgrade -y
+
+git clone https://itz-jeol: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@github.com/itz-jeol/pendingapprover Bot
+cd Bot   
+
+pip install -r requirements.txt    
+
+python3 -m main         
