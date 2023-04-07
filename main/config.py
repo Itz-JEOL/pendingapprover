4e24902d219545744fed22a73cbe53e8e2cd5034
Jeol Paul <123471314+Itz-JEOL@users.noreply.github.com>
2023-04-07 20:59:36 +0530

    Update config.py

--- main/config.py
+++ main/config.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = int(14417186)
     API_HASH = "21731d919d79f78de24bdf1f6ccd6921"
     BOT_TOKEN = "5989405319:AAEqwAGfViL0aNNykXvX0D-bPRnxQc968zQ"
-    BOT_SESSION = ""
+    BOT_SESSION = "BADb_SIAF8RLe3ZxRdUPoskbrFwF-kSUgrZ7yjHjuqEyXtd-N2ylwlxxg0FZ8fcZq-38XMfjKt_q5G2MX5eum1FsXGBY0Vh6wvdlVWVQP27TnvwnoIlEtubMgkV03MRfUVI450nItxOLju_R5YsIwbb-Lq2Csfc2EalaTvBt2wNIytki-uaTjtxI4jbVVOI7jTXcsxqjFbtyydPapRjDyyeaJBT79NZIuvZUjFijEntq0LElTCF9ZxKdqgAOI5QQivendFdZ3R8fNHmpFmINcbgCgI9Bqut6BsoZ0sUh9fItlOdg8bnqPcVIveHz4Ggf5W5TRCd_3iNvixwwhYKm-qKyzosmwQAAAAFk_xKHAQ"
 
     DB_URL = "mongodb+srv://MONGOO:MONGOO@cluster0.0a5cpqh.mongodb.net/?retryWrites=true&w=majority"
     DB_NAME = "anything"
