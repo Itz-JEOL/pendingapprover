e34f809497dd63dae9796e72f6a77ea24dc02ce5
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-04-08 01:32:57 +0530

    Create admin_panel.py

---
+++ main/bot/admin_panel.py
@@ -0,0 +1,28 @@
+from pyrogram import filters
+from main import Bot
+
+import os, sys, asyncio
+from main.config import Config 
+
+
+@Bot.on_message(filters.command("stats") & filters.user(Config.ADMINS))
+async def stats(b, m):
+    await m.reply(
+        text=f"ippo onnuilla")
+
+
+
+
+@Bot.on_message(filters.command("update") & filter.user(Config.ADMINS))      
+async def bot_up(b, m):
+    await m.reply_text("Updating........")
+    os.system("git pull")
+    asyncio.sleep(1)
+    os.execl(sys.executable, sys.executable, "-m", "main")
+ 
+
+@Bo.on_message(filters.command("restart") & filters.user(Config.ADMINS))
+async def restart(b, m):
+   await m.reply("restating.........")
+   os.execl(sys.executable, sys.executable, "-m", "main")
+ 
