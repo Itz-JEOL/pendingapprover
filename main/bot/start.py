dd0f6cb356f1135562a75040a8b058e29ba91fe9
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-04-08 13:39:07 +0530

    Update start.py

--- main/bot/start.py
+++ main/bot/start.py
@@ -1,1 +1,19 @@
+# © @MrMKN.git
 
+from pyrogram import filters
+from pyrogram.types import *
+
+from main import Bot
+from main.database import db, add_user
+from main.config import Config 
+
+
+@Bot.on_message(filters.command("start") & fliters.private)
+async def _start(b, m):
+    user = m.from_user
+  
+    await add_user(user.id)
+
+    await m.reply(
+        text=f"""Hai {user.mention} 💞"""
+    ) 
